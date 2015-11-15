58d25c1368e162b08cbfd424984557712a245345
qijun-jiang <qijun@umich.edu>
2015-11-14 22:16:33 -0500

    php read files

--- connector.php
+++ connector.php
@@ -1,7 +1,24 @@
 <?php
-$pdo = new PDO('mysql:host=35.2.112.23;dbname=584test', 'root', '110110');
+$pdo = new PDO('mysql:host=localhost;dbname=584test', 'root', 'northern');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql="CREATE TABLE test2 (x int, y int);";
-$stmt = $pdo->exec($sql);
-echo $stmt;
+
+$sqlhandle = fopen("search.sql", "r");
+
+if ($sqlhandle) {
+        $starttime = microtime(true);
+        while (($sql = fgets($sqlhandle, 4096)) !== false) {
+                $stmt = $pdo->exec($sql);
+                echo $stmt."\n";
+        }
+        if (!feof($sqlhandle)) {
+                echo "Error: unexpected fgets() fail\n";
+        }
+        //$sql="CREATE TABLE test5 (x int, y int);";
+        //$stmt = $pdo->exec($sql);
+        $endtime = microtime(true);
+        fclose($sqlhandle);
+        $timediff = $endtime - $starttime;
+        echo $timediff."\n";
+}
+
 
