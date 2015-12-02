f15bfb48adaf9ce241ebca5e1f51b36e3d0c37d6
zhangzhizhong <zhangzz@umich.edu>
2015-12-01 20:04:54 -0500

    add executor.cpp

---
+++ executor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "executor.h"
+using namespace std;
+
+
+int Executor::execute(string query_string){
+// create table
+// insert
+// query
+// drop table
+
+
+    return 0;
+}
