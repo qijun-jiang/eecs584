dd823d09a4da62dc2a95038e2c27d431245a4857
ianzyc <ianzyc@umich.edu>
2015-12-01 20:51:04 -0500

    parser

--- parser.cpp
+++ parser.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 map<string, TableData*> table_map;
 
+
+double get_double(string table_name, string attr_name) {
+	stringstream ss;
+	ss << attr_name;
+    double value;
+    if (ss >> value) {
+    	return value;
+    }
+    else {
+	    TableData* tb_data;
+	    if (table_map.find(table_name) == table_map.end()) {
+	        table_map[table_name] = new TableData(table_name);
+	    }
+	    tb_data = table_map[table_name];
+
+	    if (tb_data->string_map_.find(attr_name) == tb_data->string_map_.end()){
+	        tb_data->string_map_[attr_name] = (tb_data->string_counter_)++;
+	    }
+	    return tb_data->string_map_[attr_name];
+    }
+}
+
 //
 ParserResult Parser::Parse(string query) {
 	ParserResult result;
@@ -34,92 +56,47 @@ ParserResult Parser::Parse(string query) {
 		s_list[6].erase(s_list[6].end()-1);
 		s_list[10].erase(s_list[10].end()-1);
 		result.rect = Rect(stod(s_list[5]), stod(s_list[6]), stod(s_list[9]), stod(s_list[10]));
+
+		// attribute value
+		s_list[16].erase(s_list[16].end()-3, s_list[16].end());
+		s_list[16].erase(s_list[16].begin());
+		result.attr_value = get_double(result.table_name, s_list[16]);
 	}
-	// 	case DROP: {
-	// 		drop_table(s_list[1]);
-	// 		break;
-	// 	}
-	// 	case INSERT: {
-	// 		s_list[5].erase(s_list[5].end()-1);
-	// 		s_list[7].erase(s_list[7].end()-1);
-	// 		s_list[9].erase(s_list[9].end()-1);
-	// 		s_list[11].erase(s_list[11].end()-1);
-	// 		s_list[15].erase(s_list[15].end()-2, s_list[11].end());
-	// 		s_list[15].erase(s_list[15].begin());
+	else if (type == "select") {
+		result.table_name = s_list[3];
+		if (s_list[5] == "ST_CONTAINS") {
+			result.spatial_type = 'C';
+		}
+		else if (s_list[5] == "ST_INTERSECTS") {
+			result.spatial_type = 'I';
+		}
+		s_list[8].erase(s_list[8].end()-1);
+		s_list[12].erase(s_list[12].end()-1);
+		result.rect = Rect(stod(s_list[7]), stod(s_list[8]), stod(s_list[11]), stod(s_list[12]));
+		result.cmp_op = s_list[21][0];
 
-	// 		Polygon p;
-	// 		p.push_back(Point(stod(s_list[4]), stod(s_list[5])));
-	// 		p.push_back(Point(stod(s_list[6]), stod(s_list[7])));
-	// 		p.push_back(Point(stod(s_list[8]), stod(s_list[9])));
-	// 		p.push_back(Point(stod(s_list[10]), stod(s_list[11])));
-
-	// 		insert_query(s_list[1], p, s_list[15]);
-			
-	// 		break;
-	// 	}
-	// 	case SELECT: {
-			
-	// 	}
-
-	// 	case DELETE:
-	// 		break;
-			
-
-
-		
-	// }
-
-
-	
+		// attribute value
+		s_list[22].erase(s_list[22].end()-2, s_list[22].end());
+		s_list[22].erase(s_list[22].begin());
+		result.attr_value = get_double(result.table_name, s_list[22]);
+	}
 	return result;
-
-
-
-
-
-
-
-
-
-
-
 }
 
 int main() {
-
-
-	
-
-    string table_name = "gis";
-    TableData* tb_data;
-    if (table_map.find(table_name) == table_map.end()){
-        table_map[table_name] = new TableData(table_name);
-    }
-    tb_data = table_map[table_name];
-
-    string attr_name = "blue";
-    if (tb_data->string_map_.find(attr_name) == tb_data->string_map_.end()){
-        tb_data->string_map_[attr_name] = (tb_data->string_counter_)++;
-    }
-    double attr_number = tb_data->string_map_[attr_name];
-
-
 	Parser parser;
-
-
+	parser.Parse("SELECT COUNT(*) FROM gis1 WHERE ST_CONTAINS (PolygonFromText('POLYGON(( 24585.6 8324.77, 34585.6 8324.77, 34585.6 18324.8, 24585.6 18324.8, 24585.6 8324.77 ))'), g) AND color = 'red';");
 	parser.Parse("INSERT INTO gis1 VALUES (PolygonFromText('POLYGON(( 89031.9 55459.7, 89147.3 55459.7, 89147.3 55575.1, 89031.9 55575.1, 89031.9 55459.7 ))'), 'blue');");
 
-
-	// parser("CREATE TABLE gis1 (g GEOMETRY NOT NULL, color VARCHAR(12), SPATIAL INDEX(g)) ENGINE=MyISAM;");
-
-	// parser("CREATE TABLE gis (g GEOMETRY NOT NULL, color VARCHAR(12), SPATIAL INDEX(g)) ENGINE=MyISAM;\n");
-	// parser("INSERT INTO gis VALUES\n(PolygonFromText('POLYGON(( 10 10, 20 10, 20 20, 10 20, 10 10 ))'), 'red')\n");
-
-// 	parser("SELECT COUNT(*) FROM gis1 WHERE ST_CONTAINS(PolygonFromText('POLYGON((";
-// 		for (int j = 0; j <= 3; j++) {
-// 			fs1 << p[j].first << " " << p[j].second << ", ";
-// 		}
-// 		fs1 << p[0].first << " " << p[0].second << "))'), g) AND ";
-// 		fs1 << "color='red';\n";
-// }
+	// for (auto it = table_map["gis1"]->string_map_.begin(); it != table_map["gis1"]->string_map_.end(); it++) {
+	// 	cout << it->first << " " << it->second << endl;
+	// }
 }
+
+
+
+
+
+
+
+
