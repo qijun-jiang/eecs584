7d7800352afb1be25e62204a01a588300514ef25
ianzyc <ianzyc@umich.edu>
2015-12-01 19:29:06 -0500

    Create parser.h

---
+++ parser.h
@@ -0,0 +1,44 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <unordered_map>
+#include "table_data.h"
+
+using namespace std;
+
+// typedef pair<double, double> Point;
+// typedef vector<Point> Polygon;
+
+struct ParserResult {
+	string query_type;
+	string table_name;
+	Rect rect;
+	char spatial_type;
+	char cmp_op;
+	bool is_double;
+	double attr_value;
+	string attr_string;
+
+	ParserResult() {}
+};
+
+class Parser {
+
+public:
+	ParserResult Parse(string query);
+
+};
+
+
+#endif
+
+
+
